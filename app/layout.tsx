6e4cf87739623b196a055fe86fc6c70dc6e3f16d
youaresofunny <anlopan@gmail.com>
2025-11-03 20:23:23 +0500

    metrics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import '@livekit/components-styles';
 import '@livekit/components-styles/prefabs';
 import '../styles/globals.css';
 import type { Metadata, Viewport } from 'next';
+import Script from 'next/script';
 import { Toaster } from 'react-hot-toast';
 
 export const metadata: Metadata = {
@@ -43,6 +44,27 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ru">
       <body data-lk-theme="default">
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105099728', 'ym');
+
+            ym(105099728, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/105099728"
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+            />
+          </div>
+        </noscript>
         <Toaster />
         {children}
       </body>
